4911b207df21c7b8dac42e5d7291b613f4025648
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-10-10 16:56:52 +0700

    feat: add admin middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!auth()->check() || auth()->user()->email !== 'admin@gmail.com') {
+            abort(403, 'Unauthorized action.');
+        }
+
+        return $next($request);
+    }
+}
