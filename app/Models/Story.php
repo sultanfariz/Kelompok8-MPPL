f3bf05110715d3d4775fb82fdae59cc6d8ab7504
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-10-12 22:44:24 +0700

    feat: add Story model

---
+++ app/Models/Story.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Story extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'body', 'image', 'like_count', 'comment_count'
+    ];
+}
---
+++ database/migrations/2021_10_12_153250_create_stories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->string('image');
+            $table->string('like_count');
+            $table->string('comment_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+}
