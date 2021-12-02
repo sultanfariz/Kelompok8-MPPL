f79880cea06d8f7c79fecc2d78013d5baadf09ee
fachirah_ama <fachirah.ama@gmail.com>
2021-12-02 10:18:45 +0700

    buku terfav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -206,7 +206,7 @@
                     anak. Mereka lalu berdoa agar segera dapat menimang anak…
                   </p>
                   <a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
-                      href="#">
+                      href="/story/1">
                       Read more
                   </a>
               </div>
